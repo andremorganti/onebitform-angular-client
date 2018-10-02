269fa4a36b7e0ea2079dccad1cee52971af49110
Andre Morganti <acmorganti@gmail.com>
2018-10-02 13:28:05 -0300

    routing created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule } from './forms/forms.module';
 import { AnswersModule } from './answers/answers.module';
+import { RouterModule } from '@angular/router';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { AnswersModule } from './answers/answers.module';
     ColorPickerModule,
     ChartsModule,
     FormsModule,
-    AnswersModule
+    AnswersModule,
+    RouterModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
